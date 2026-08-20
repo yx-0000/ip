afae4f539385be4e3e1fdc04969d8bf6918bff64
yuxin <227103897+yx-0000@users.noreply.github.com>
2026-08-20 20:51:34 +0800

    level-1

--- src/main/java/TT.java
+++ src/main/java/TT.java
@@ -2,19 +2,43 @@ import java.util.Scanner;
 
 public class TT {
     public static void main(String[] args) {
-        String line = "________________________________________ \n";
-        String banner = line
-                + " ____        _        \n"
+
+        String divider = "________________________________________ \n";
+        String banner = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
-        String intro = "Hello! I'm TT.\n" + "What can I do for you? \n" + line;
-        String bye = "Bye. Hope to see you again soon!\n" + line;
+        String intro = "Hello! I'm TT.\n" + "What can I do for you?";
+        String bye = "Bye. Hope to see you again soon!";
 
+        System.out.println(divider);
         System.out.println(banner);
-        System.out.println(intro + bye);
-        //System.out.println(bye);
+        System.out.println(intro);
+        System.out.println(divider);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            String input = scanner.nextLine();
+
+            if(input.equals("bye")) {
+                System.out.println(divider + bye);
+                System.out.println(divider);
+                break;
+            }
+
+            if (input.equals("list")) {
+                System.out.println(divider + "list");
+                System.out.println(divider);
+                break;
+            }
+
+            System.out.println(input);
+            System.out.println(divider);
+        }
+
+        scanner.close();
     }
 }
